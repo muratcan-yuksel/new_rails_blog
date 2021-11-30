1e3f47021890252291bb22a429a1bc028e5d0c3d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-30 09:24:36 +0300

    change navbar display settings

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
   # do certain things only after authentication
 
-  # before_action :authenticate_auth!, except: [:index, :show]
+  before_action :authenticate_auth!, except: [:index, :show]
 
 
   # GET /posts or /posts.json
