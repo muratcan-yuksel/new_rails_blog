0110a8305da772b85312c707180d14ea76862d70
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-30 03:39:26 +0300

    .

---
+++ db/migrate/20211130003854_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :category
+      t.string :author
+      t.text :content
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
